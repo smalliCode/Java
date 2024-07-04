e24946ec930d6d28f548133b3ba6e956f84af90e
ismael <ismael2024-tech@outlook.com>
2024-07-03 22:18:22 -0300

    Adicionando o tipo de dado int e float

--- Java.java
+++ Java.java
@@ -14,6 +14,22 @@ public class Java{
         
         System.out.println(nome);// => mostrando o valor da nossa varivel, utilizando o println.
 
+        /*
+         * int => tipo de dado primitivo que salva numeros inteiros em uma faixa de valores.
+         * A partir daqui vamos apenas descrever os tipos, ja que a definiçao de nome e valor sao os mesmos.
+         */
+        int numero;
+        numero = 3;
+
+        System.out.println(numero);
+
+        /*
+         * float => tipo de dado primitivo que salva numeros reais em uma faixa de valores.
+         * O tipo float acompanha a letra 'f' em minusculo no final do seu valor.
+         */
+        float numeroReal = 3.5f; // => podemos fazer a criaçao e atribuiçao ao mesmo tempo.
+
+        System.out.println(numeroReal);
 
     }
 }
