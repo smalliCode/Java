7379b10c430186a4d1d92fdd0dc59dcf9dc6978f
ismael <ismael2024-tech@outlook.com>
2024-07-03 22:06:26 -0300

    Aprendendo sobre variaveis, String

--- Java.java
+++ Java.java
@@ -1,6 +1,19 @@
 public class Java{
 
     public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
+
+
+        /*
+        * Variavel, tipos e valor:
+        * String => tipo da variavel; nome => nome da variavel; aluno => valor da variavel.
+        * Em java, String é uma classe que serve para armazenar textos, simbolos e espaços.
+        */
+        String nome ; // => criando uma variavel.
+        nome = "Aluno";// => atribuindo valor a variavel.
+
+        
+        System.out.println(nome);// => mostrando o valor da nossa varivel, utilizando o println.
+
+
     }
 }
